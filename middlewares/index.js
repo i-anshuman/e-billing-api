aa6a09c013a21692e649e22474f8e986cb385462
Anshuman Gupta <anshuman844@gmail.com>
2020-10-19 21:43:03 +0530

    feat: Add notFound, watchErrors & one more methods 1. notFound - to display "404 not found" message. 2. watchErrors - to validate input and display any validation error. 3. ensureAuthenticated - to check if user is authenticated or not.

---
+++ middlewares/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const notFound = (req, res) => {
+  res.status(404).json({ error: "404 Not Found!!" });
+};
+
+const watchErrors = (schema, request) => {
+  const validationOptions = {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true
+  };
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req[request], validationOptions);
+      next();
+    }
+    catch (error) {
+      const customErrors = {};
+      error.details.map(({ message, context }) => {
+        customErrors[context.key] = message;
+      });
+      return res.status(422).json({ errors: customErrors });
+    }
+  };
+};
+
+const ensureAuthenticated = (req, res, next) => {
+  req.signedCookies.token
+    ? verify(req.signedCookies.token)
+        .then(data => {
+          res.locals.user = data;
+          next();
+        })
+        .catch(error => res.status(401).json({ error: "Unauthenticated", desc: error }))
+    : res.status(401).json({ error: "Unauthenticated" });
+};
+
+module.exports = { notFound, watchErrors, ensureAuthenticated };
