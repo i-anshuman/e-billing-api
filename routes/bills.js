8709f1ac8f663f773fff75751cadca250040069e
Anshuman Gupta <anshuman844@gmail.com>
2020-10-24 15:50:29 +0530

    feat: Add '/find' route

--- routes/bills.js
+++ routes/bills.js
@@ -2,8 +2,8 @@
 
 const router = require('express').Router();
 const { watchErrors } = require('../middlewares');
-const { billSchema, billID } = require('../validators/bills');
 const { create, list, view } = require('../controllers/bills');
+const { billSchema, billID, filterBillsSchema } = require('../validators/bills');
 
 router.post('/new', watchErrors(billSchema, "body"), (req, res) => {
   create(res.locals.user._id, req.body, (error, bill) => {
@@ -31,5 +31,34 @@ router.get('/view/:billID',
     });
   }
 );
+
+router.get('/find', watchErrors(filterBillsSchema, "body"), (req, res) => {
+  const filters = { ...req.body };
+  const { date } = filters;
+  if (date) {
+    const d = {};
+    switch (typeof(date)) {
+      case "object":
+        for (const key in date) {
+          d[`$${key}`] = date[key];
+        }
+        break;
+      case "string":
+        const gt = new Date(date);
+        d[`$gt`] = gt;
+        d[`$lt`] = new Date(date).setDate(gt.getDate()+1);
+        break;
+    }
+    filters.date = d;
+  }
+  list(res.locals.user._id,
+    filters,
+    (error, bills) => {
+      error
+        ? res.status(400).json({error})
+        : res.status(201).json({bills});
+    }
+  );
+});
 
 module.exports = router;
